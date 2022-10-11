1ea76de8ca5849b58af3570edf748338f3674fcb
waliulhasaniram <waliulhasaniram000@gmail.com>
2022-10-11 20:52:57 +0600

    Hospital Management System

---
+++ Hospital Management System/Awebsite/home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from home.models import Contacts
+from home.models import Signup
+
+# Register your models here.
+admin.site.register(Contacts)
+admin.site.register(Signup)
